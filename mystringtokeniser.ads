c2a6557695a90cfce5accaf51d93d6db1f202e0b
Angus Hudson <ahudson1@student.unimelb.edu.au>
2020-06-02 12:02:52 +1000

    Task 1.1 completed

--- mystringtokeniser.ads
+++ mystringtokeniser.ads
@@ -21,5 +21,25 @@ package MyStringTokeniser with SPARK_Mode is
           Tokens(Index).Length > 0) and then
             Tokens(Index).Length-1 <= S'Last - Tokens(Index).Start);
 
+      -- Count <= Tokens'Length: This says that 'Count', the variable that tracks the number
+      -- of tokens generated in Tokenise, does not exceed 'Tokens'Length', the length of 
+      -- the array inputted to store Tokens. Necessary since Count > Tokens'Length implies
+      -- that more tokens have been generated than Tokens can store, thus there is a memory leak.
+
+      -- For all Index in Tokens'First..Tokens'First+(Count-1): For all filled indexes in the 
+      -- Token array. Necessary so all tokens are checked, instead of a subset or NULL elements.
+ 
+      -- Tokens(Index).Start >= S'First: This says that the starting index of Tokens(index) string lies
+      -- somewhere after the starting index of the input string, S'First. Necessary to ensure all tokens
+      -- began within the input string S.
+
+      -- Tokens(Index).Length-1 <= S'Last - Tokens(Index).Start: This says that the length of Tokens(Index) 
+      -- is not greater than S'Last - Tokens(Index).Start + 1. Necessary to ensure no token exceeded the 
+      -- length of the input string S. Combined with previous condition, ensures all tokens are entirely 
+      -- derived from the input string S.
+
+      -- Tokens(Index).Length > 0: This says that the length fo Tokens(Index) is a value greater than 0, 
+      -- this ensures all tokens feature non-empty substrings of the input string S.
+   
 
 end MyStringTokeniser;
